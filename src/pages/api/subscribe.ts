4d9b9e7b1f564c8d7a290d0011b9d60bcb4ebb64
Daisy <daisy@vizir.com.br>
2021-04-10 14:00:16 -0300

    feat: add sripe checkout session

---
+++ src/pages/api/subscribe.ts
@@ -0,0 +1,68 @@
+import { query as q } from "faunadb";
+import { NextApiRequest, NextApiResponse } from "next";
+import { getSession } from 'next-auth/client'
+import { fauna } from "../../services/fauna";
+import { stripe } from "../../services/stripe";
+
+type FaunaUser = {
+  ref: {
+    id: string
+  },
+  data: {
+    stripe_customer_id: string
+  }
+}
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  if (req.method === 'POST') {
+    const { user: sessionUser } = await getSession({ req }) // pega a sessão dos cookies
+
+    const faunaUser = await fauna.query<FaunaUser>(
+      q.Get(
+        q.Match(
+          q.Index('user_by_email'),
+          q.Casefold(sessionUser.email)
+        )
+      )
+    )
+
+    let customerId = faunaUser.data.stripe_customer_id
+
+    if (!customerId) {
+      const stripeCustomer = await stripe.customers.create({
+        email: sessionUser.email
+      })
+
+      await fauna.query(
+        q.Update(
+          q.Ref(q.Collection('users'), faunaUser.ref.id),
+          {
+            data: {
+              stripe_customer_id: stripeCustomer.id
+            }
+          }
+        )
+      )
+
+      customerId = stripeCustomer.id
+    }
+
+    const stripeCheckoutSession = await stripe.checkout.sessions.create({
+      customer: customerId,
+      payment_method_types: ['card'],
+      billing_address_collection: 'required',
+      line_items: [
+        { price: 'price_1IbXUhGGWkd4y9jm0NkLCmNg', quantity: 1 }
+      ],
+      mode: 'subscription',
+      allow_promotion_codes: true,
+      success_url: process.env.STRIPE_SUCCESS_URL,
+      cancel_url: process.env.STRIPE_CANCEL_URL
+    })
+
+    return res.status(200).json({ sessionId: stripeCheckoutSession.id })
+  } else {
+    res.setHeader('Allow', 'POST')
+    res.status(405).end('Method not allowed')
+  }
+}
